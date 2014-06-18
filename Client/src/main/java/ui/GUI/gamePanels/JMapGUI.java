9d0bf0e56d6f327b591e3ec74a4ef920fe09d394
Stefan B <Stefan.bie@live.de>
2014-06-18 21:34:02 +0200

    - Docs

--- Client/src/main/java/ui/GUI/gamePanels/JMapGUI.java
+++ Client/src/main/java/ui/GUI/gamePanels/JMapGUI.java
@@ -18,14 +18,31 @@ import java.awt.event.MouseEvent;
 import java.util.HashMap;
 
 /**
- * Created by Stefan on 09.06.14.
+ * Klasse Die zur Darstellung der Karte gedacht ist
  */
 public class JMapGUI extends JComponent {
 
+    /**
+     * Kartenobjekt der GameEngeine
+     * @see main.java.logic.data.Map
+     */
     private final Map map;
+
+    /**
+     * Spiel aus der GameEngine
+     * @see main.java.logic.Game
+     */
     private final Game game;
+
+    /**
+     * Die Map Loader Klasse
+     */
     private final MapLoader mapLoader;
 
+    /**
+     * Klasse, die beim Anklicken der Karte ausgelöst wird
+     * @see java.awt.event.ActionListener
+     */
     public class OnCountryClickActionListener extends MouseAdapter  {
 
         @Override
@@ -55,6 +72,11 @@ public class JMapGUI extends JComponent {
     }
 
 
+    /**
+     * Klasse, die zur Darstellung einer Karte dient
+     * @param game Spiel des der GameEngine
+     *             @see main.java.logic.Game
+     */
     public JMapGUI(Game game){
         super();
         this.game = game;
@@ -63,6 +85,10 @@ public class JMapGUI extends JComponent {
         inititize();
     }
 
+    /**
+     * Initialisiert die Karte
+     *
+     */
     private void inititize (){
         Dimension dim = new Dimension(600,400);
         this.addMouseListener(new OnCountryClickActionListener());
@@ -72,7 +98,14 @@ public class JMapGUI extends JComponent {
         this.setMinimumSize(dim);
         this.setSize(dim);
         this.setLayout(null);
+
     }
+
+    /**
+     * Beschreibt wie die Karte gezeichnet werden soll
+     * @param g
+     */
+    @Override
     public void paint(Graphics g){
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
@@ -83,6 +116,7 @@ public class JMapGUI extends JComponent {
 
         //Land Infos
 
+        //Immer wieder erneut löschen und hinzufügen ist am performantesten und auch am besten Wartbar
         this.removeAll();
         HashMap<Point,Country> postions = this.mapLoader.getCountryInfoCoordinates(this.getWidth(),this.getHeight());
         for(java.util.Map.Entry entry : postions.entrySet()){
@@ -100,9 +134,14 @@ public class JMapGUI extends JComponent {
         g2.drawImage(this.mapLoader.getFrontImage(), 0, 0, this.getWidth(), this.getHeight(), null);
         super.paint(g);
 
-
     }
 
+    /**
+     * Gett für Country an einer bestimmten x,y Position
+     * @param x Position auf der X-Achse
+     * @param y Position auf der Y-Achse
+     * @return Land das sich an der (x,y) Position befindet.
+     */
     public Country getCountry(int x, int y){
         return this.mapLoader.getCountry(x,y, this.getWidth(),this.getHeight());
     }
