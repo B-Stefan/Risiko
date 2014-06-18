0503c81b0fee06b2b75ee215c136bc1e856dd35d
Stefan B <Stefan.bie@live.de>
2014-06-18 16:26:23 +0200

    - Fight Click Action

--- Client/src/main/java/ui/GUI/country/JCountryFightMenu.java
+++ Client/src/main/java/ui/GUI/country/JCountryFightMenu.java
@@ -1,10 +1,13 @@
 package main.java.ui.GUI.country;
+import main.java.logic.Fight;
 import main.java.logic.Turn;
 import main.java.logic.data.Country;
 import main.java.logic.exceptions.*;
 import main.java.ui.GUI.utils.JExceptionDialog;
 import main.java.ui.GUI.utils.JModalDialog;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,6 +17,7 @@ import java.awt.event.ActionListener;
 public class JCountryFightMenu extends JCountryNeighborsMenu {
 
     private final Turn turn;
+    private final Country country;
     public class NeighborActionListener implements ActionListener{
 
         /**
@@ -24,13 +28,24 @@ public class JCountryFightMenu extends JCountryNeighborsMenu {
         @Override
         public void actionPerformed(ActionEvent event) {
             if(event.getActionCommand() == "onCountryClick"){
-                new JExceptionDialog(JCountryFightMenu.this,"Nicht implementiert bis jetzt");
-                //TODO Erstellen
+                Country from    = JCountryFightMenu.this.country;
+                Country to      =  JCountryFightMenu.this.getSelectedNeighborsMenuItem().getCountry();
+                Fight fight;
+                try {
+                    fight = JCountryFightMenu.this.turn.fight(from, to);
+                }catch (TurnNotInCorrectStepException | TurnNotAllowedStepException | ToManyNewArmysException | NotTheOwnerException e ){
+                    new JExceptionDialog(JCountryFightMenu.this,e);
+                    return;
+                }
+                JPopupMenu menu = (JPopupMenu) JCountryFightMenu.this.getParent();
+                JModalDialog modal = new JFightGUI(menu.getInvoker(),fight);
+
             }
         }
     }
     public JCountryFightMenu(final Country country, final Turn turn){
         super("Fight",country);
+        this.country = country;
         this.turn = turn;
         this.addActionListener(new NeighborActionListener());
 
